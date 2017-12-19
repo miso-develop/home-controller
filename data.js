e138d48204b0ca32802da7a2476b59bd0540108e
miso <miso.develop@gmail.com>
2017-12-19 23:07:35 +0900

    ver1.0

---
+++ data.js
@@ -0,0 +1,98 @@
+const data = [
+	{
+		"name": "電気",
+		"buttons": [
+			{"name": "A", "command": "light a", "column": "2"}, 
+			{"name": "B", "command": "light b", "column": "2"}, 
+			{"name": "C", "command": "light c", "column": "2"}, 
+			{"name": "D", "command": "light d", "column": "2"}, 
+			{"name": "全灯", "command": "light 起動", "column": "3"}, 
+			{"name": "保安灯", "command": "light 保安灯", "column": "3"}, 
+			{"name": "消灯", "command": "light 停止", "column": "3"}, 
+		]
+	},
+	
+	{
+		"name": "TV",
+		"buttons": [
+			{"name": "電源", "command": "tv 起動", "column": "2"}, 
+			{"name": "入力切替", "command": "tv 入力切替", "column": "2"}, 
+			{"name": "音量アップ", "command": "tv 音量 アップ", "column": "2"}, 
+			{"name": "音量ダウン", "command": "tv 音量 ダウン", "column": "2"}, 
+			{"name": "1", "command": "tv 1", "column": "3"}, 
+			{"name": "2", "command": "tv 2", "column": "3"}, 
+			{"name": "3", "command": "tv 3", "column": "3"}, 
+			{"name": "4", "command": "tv 4", "column": "3"}, 
+			{"name": "5", "command": "tv 5", "column": "3"}, 
+			{"name": "6", "command": "tv 6", "column": "3"}, 
+			{"name": "7", "command": "tv 7", "column": "3"}, 
+			{"name": "8", "command": "tv 8", "column": "3"}, 
+			{"name": "9", "command": "tv 9", "column": "3"}, 
+			{"name": "10", "command": "tv 10", "column": "3"}, 
+			{"name": "11", "command": "tv 11", "column": "3"}, 
+			{"name": "12", "command": "tv 12", "column": "3"}, 
+			{"column": "3"},
+			{"name": "↑", "command": "tv 上", "column": "3"}, 
+			{"column": "3"},
+			{"name": "←", "command": "tv 左", "column": "3"}, 
+			{"name": "↓", "command": "tv 下", "column": "3"}, 
+			{"name": "→", "command": "tv 右", "column": "3"}, 
+			{"name": "決定", "command": "tv 決定", "column": "2"}, 
+			{"name": "戻る", "command": "tv 戻る", "column": "2"}, 
+		],
+	},
+	
+	{
+		"name": "PS4",
+		"buttons": [
+			{"name": "起動", "command": "ps4 起動", "column": "2"}, 
+			{"name": "停止", "command": "ps4 停止", "column": "2"}, 
+			{"name": "ホーム", "command": "ps4 ホーム", "column": "2"}, 
+			{"name": "オプション", "command": "ps4 オプション", "column": "2"}, 
+			{"name": "決定", "command": "ps4 enter", "column": "2"}, 
+			{"name": "バック", "command": "ps4 戻る", "column": "2"}, 
+			{"column": "3"},
+			{"name": "↑", "command": "ps4 上", "column": "3"}, 
+			{"column": "3"},
+			{"name": "←", "command": "ps4 左", "column": "3"}, 
+			{"name": "↓", "command": "ps4 下", "column": "3"}, 
+			{"name": "→", "command": "ps4 右", "column": "3"}, 
+			{"name": "torne", "command": "ps4 トルネ", "column": "2"}, 
+			{"name": "メディア", "command": "ps4 メディア", "column": "2"}, 
+		],
+	},
+	
+	{
+		"name": "PC",
+		"buttons": [
+			{"name": "起動", "command": "pc 起動", "column": "1"}, 
+			{"name": "スタンバイ", "command": "pc スタンバイ", "column": "1"}, 
+			{"name": "モニタオフ", "command": "pc 画面 オフ", "column": "1"}, 
+			{"name": "Excel", "command": "pc Excel", "column": "1"}, 
+		],
+	},
+	
+	{
+		"name": "エアコン",
+		"buttons": [
+			{"name": "暖房", "command": "aircon 暖房", "column": "4"}, 
+			{"name": "冷房", "command": "aircon 冷房", "column": "4"}, 
+			{"name": "除湿", "command": "aircon 除湿", "column": "4"}, 
+			{"name": "送風", "command": "aircon 送風", "column": "4"}, 
+			{"name": "温度アップ", "command": "aircon 温度アップ", "column": "2"}, 
+			{"name": "温度ダウン", "command": "aircon 温度ダウン", "column": "2"}, 
+			{"name": "風速", "command": "aircon 風速", "column": "2"}, 
+			{"name": "風向き", "command": "aircon 風向き", "column": "2"}, 
+			{"name": "停止", "command": "aircon 停止", "column": "1"}, 
+		],
+	},
+	
+	{
+		"name": "Notifier",
+		"buttons": [
+			{"name": "部屋の温度", "command": "notifier 部屋の温度", "column": "1"}, 
+			{"name": "チューニング", "command": "notifier チューニング", "column": "1"}, 
+		],
+	},
+	
+]
---
+++ main.js
@@ -0,0 +1,321 @@
+"use strict"
+const l = v => console.log(v)
+
+
+
+// firebase
+let ref; if (typeof firebase !== "undefined") ref = firebase.database().ref("/googlehome")
+
+// sp max width(16x36)
+const spMaxWidth = 576
+const device = window.innerWidth <= spMaxWidth ? "sp" : "pc"
+
+
+
+// create dom
+const createDom = () => {
+	// document fragment
+	let tabs = document.createDocumentFragment()
+	let buttons = document.createDocumentFragment()
+	
+	// category loop
+	for (let category of data) {
+		// create tab box
+		let tabBox = document.createElement("div")
+		tabs.appendChild(tabBox)
+		tabBox.className = "tab-box"
+		// create tab
+		let tab = document.createElement("button")
+		tabBox.appendChild(tab)
+		tab.className = "tab"
+		tab.innerHTML = category.name
+		tab.addEventListener("click", function() {selectTab(this)})
+		
+		// create button group
+		let buttonGroup = document.createElement("div")
+		buttons.appendChild(buttonGroup)
+		buttonGroup.className = "button-group"
+		buttonGroup.setAttribute("data-group", category.name)
+		
+		// button data loop
+		for (let buttonData of category.buttons) {
+			// create button box
+			let buttonBox = document.createElement("div")
+			buttonGroup.appendChild(buttonBox)
+			buttonBox.className = "button-box"
+			buttonBox.style.width = 100 / buttonData.column + "%"
+			
+			// check blank
+			if (buttonData.name) {
+				// create button
+				let button = document.createElement("button")
+				buttonBox.appendChild(button)
+				button.className = "button"
+				button.innerHTML = buttonData.name
+				button.addEventListener("click", () => run(buttonData))
+			}
+		}
+	}
+	
+	// Notifierにtextbox追加
+	const notifierGroup = buttons.querySelector(`.button-group[data-group="Notifier"]`)
+	if (notifierGroup) {
+		const textBox = document.createElement("div")
+		notifierGroup.insertBefore(textBox, notifierGroup.firstChild)
+		textBox.className = "text-box"
+		//
+		const text = document.createElement("input")
+		textBox.appendChild(text)
+		text.type = "text"
+		text.placeholder = "enter the free words!"
+		//
+		const button = document.createElement("button")
+		textBox.appendChild(button)
+		button.className = "button"
+		button.innerHTML = "Notice"
+		button.addEventListener("click", () => {
+			run({"name": "Notice", "command": `notifier ${text.value}`})
+			text.value = ""
+		})
+	}
+	
+	// 最初の要素選択
+	tabs.querySelector(".tab").classList.toggle("tab-selected")
+	buttons.querySelector(".button-group").classList.toggle("button-group-selected")
+	
+	// dom反映
+	document.getElementById("tabs").appendChild(tabs)
+	document.getElementById("buttons").appendChild(buttons)
+}
+
+// button click
+const run = buttonData => {
+	//firebase command
+	if (typeof buttonData.command === "string") {
+		l(buttonData.command)
+		if (typeof ref !== "undefined") ref.set({"word": buttonData.command})
+	//setting tab
+} else if (typeof buttonData.command === "object") {
+		settings.tabPosition[device].set(buttonData.command.tabPosition)
+	}
+	//toast
+	toast(buttonData.name)
+}
+
+// tab click
+const selectTab = tab => {
+	// .tab-selected付け替え
+	document.getElementsByClassName("tab-selected")[0].classList.toggle("tab-selected")
+	tab.classList.toggle("tab-selected")
+	// button group表示切り替え
+	document.getElementsByClassName("button-group-selected")[0]
+		.classList.toggle("button-group-selected")
+	document.querySelector(`.button-group[data-group="${tab.innerHTML}"]`)
+		.classList.toggle("button-group-selected")
+	// fadein
+	fadein(document.querySelector(`.button-group[data-group="${tab.innerHTML}"]`), 250)
+}
+
+// fadein
+const fadein = (elem, fadeTime) => {
+	elem.style.opacity = 0
+	elem.style.zindex = 0
+	const begin = new Date();
+	var id = setInterval(function() {
+		var current = new Date() - begin
+		if (current > fadeTime){
+			clearInterval(id)
+			current = fadeTime
+		}
+		elem.style.opacity = current / fadeTime
+		elem.style.zIndex = 0
+	}, 10);
+}
+// fadeout
+const fadeout = (elem, fadeTime) => {
+	elem.style.opacity = 100
+	elem.style.zindex = 0
+	const begin = new Date()
+	var id = setInterval(function() {
+		var current = fadeTime - (new Date() - begin)
+		if (current <= 0){
+			clearInterval(id)
+			current = 0
+		}
+		elem.style.opacity = current / fadeTime
+	}, 10);
+}
+
+//toast
+const toast = message => {
+	const toast = document.getElementById("toast")
+	const toastMessage = document.createElement("div")
+	toastMessage.innerHTML = message
+	toast.innerHTML = ""
+	toast.appendChild(toastMessage)
+	fadein(toastMessage, 250)
+	setTimeout(() => fadeout(toastMessage, 500), 1500)
+}
+
+
+
+// スワイプでタブ切替
+const setSwipeEvent = () => {
+	// set
+	const set = (eventData) => {
+		// start event
+		document.addEventListener(eventData.startEvent, event => {
+			if (exclusionArea(event)) return
+			this.startX = eventData.startX(event)
+		})
+		
+		// end event
+		document.addEventListener(eventData.endEvent, event => {
+			if (exclusionArea(event)) return
+			this.endX = eventData.endX(event)
+			swipe()
+		})
+		
+		// タブ領域を除外
+		const exclusionArea = event => event.target.offsetParent && (
+			(event.target.id || event.target.offsetParent.id) === "tabs" || 
+			(event.target.className || event.target.offsetParent.className) === "text-box" 
+		)
+		
+		// スワイプ動作実装
+		const swipe = () => {
+			// scroll size(16*6)
+			const scrollSize = 96
+			const margin = 50
+			
+			// swipe left
+			if (this.startX > (this.endX + margin)) {
+				// tab scroll right
+				document.getElementById("tabs").scrollBy(scrollSize, 0)
+				// tab right shift
+				let flag = false
+				for (const tab of document.getElementsByClassName("tab")) {
+					if (flag) return selectTab(tab)
+					if (tab.classList.contains("tab-selected")) flag = true
+				}
+			
+			// swipe right
+			} else if ((this.startX + margin) < this.endX) {
+				// tab scroll left
+				document.getElementById("tabs").scrollBy(scrollSize * -1, 0)
+				// tab left shift
+				let prevTab = document.getElementsByClassName("tab-selected")[0]
+				for (const tab of document.getElementsByClassName("tab")) {
+					if (tab.classList.contains("tab-selected")) selectTab(prevTab)
+					prevTab = tab
+				}
+				
+			}
+		}
+	}
+	// touch event
+	set({
+		"startEvent" : "touchstart",
+		"startX": event => event.touches[0].pageX,
+		"endEvent": "touchend",
+		"endX": event => event.changedTouches[0].pageX,
+	})
+	// mouse event
+	set({
+		"startEvent" : "mousedown", 
+		"startX": event => event.screenX,
+		"endEvent": "mouseup",
+		"endX": event => event.screenX,
+	})
+}
+
+
+
+// 設定
+class Settings {
+	constructor(data) {
+		// jsonに設定情報追加
+		data.push({
+			"name": "Setting",
+			"buttons": [
+				{"name": "タブ位置：上", "command": {"tabPosition": "top"}, "column": "1"},
+				{"name": "タブ位置：下", "command": {"tabPosition": "bottom"}, "column": "1"},
+			]
+		})
+		
+		// debug
+		// this.clear()
+		
+		// 初期化
+		this.init()
+		return this
+	}
+	
+	// 初期化
+	init() {
+		let settings = {
+			"tabPosition": {
+				"sp": {"value": "bottom"},
+				"pc": {"value": "top"}
+			}
+		}
+		if (localStorage.settings) settings = this.load()
+		// settingsにsetter追加
+		this.addSetter(settings)
+		// オブジェクトマージ
+		Object.assign(this, settings)
+		// タブ設定を画面に反映
+		this.setTabPosition()
+	}
+	
+	// settingsにsetter追加
+	addSetter(obj) {
+		const settings = this
+		for (let prop of Object.keys(obj)) {
+			if (typeof obj[prop] !== "object") return
+			this.addSetter(obj[prop])
+			obj[prop].set = function(value) {
+				this.value = value
+				settings.save()
+			}
+		}
+	}
+	
+	// タブ設定を画面に反映
+	setTabPosition() {
+		document.getElementById("main").className = `tab-${this.tabPosition[device].value}`
+	}
+	
+	// 設定値をlocalStrageへ書き込み
+	save() {
+		localStorage.settings = JSON.stringify(this, (k, v) => 
+			typeof v === "function" ? v.toString() : v)
+		// // タブ設定を画面に反映
+		this.setTabPosition()
+	}
+	
+	// 設定値をlocalStrageから取得
+	load() {
+		return JSON.parse(localStorage.settings, (k, v) => 
+			typeof v === "string" && (v.match(/^function/) || v.match(/=>/)) ? 
+				Function.call(this, "return "+ v  )() : v)
+	}
+	
+	// localStorageクリア
+	clear() {localStorage.removeItem("settings")}
+}
+
+
+
+// 設定
+const settings = new Settings(data)
+
+// content loaded
+document.addEventListener("DOMContentLoaded", event => {
+	
+	// dom生成
+	createDom()
+	// スワイプでタブ切替
+	setSwipeEvent()
+	
+})
